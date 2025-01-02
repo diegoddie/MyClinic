0f7e62ea7092d36a0a07d141c3dea5a06bdcfe77
Diego <diego.boost@gmail.com>
2025-01-02 19:58:36 +0100

    fix

---
+++ components/utils/ServiceCard.tsx
@@ -0,0 +1,54 @@
+import React from "react";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Button } from "../ui/button";
+
+interface ServiceCardProps {
+  icon: React.ReactNode;
+  title: string;
+  description: string;
+  treatments?: string[];
+}
+
+function ServiceCard({
+  icon,
+  title,
+  description,
+  treatments,
+}: ServiceCardProps) {
+  return (
+    <Card className="flex flex-col h-full">
+      <CardHeader className="space-y-4">
+        <div className="flex items-center space-x-4">
+          {icon}
+          <CardTitle className="text-primary text-lg md:text-xl">
+            {title}
+          </CardTitle>
+        </div>
+        <CardDescription className="text-paragraphs">
+          {description}
+        </CardDescription>
+      </CardHeader>
+      
+      <CardContent className="flex-grow">
+        <ul className="list-disc list-inside space-y-1">
+          {treatments?.map((treatment, index) => (
+            <li key={index} className="text-sm text-paragraphs">
+              {treatment}
+            </li>
+          ))}
+        </ul>
+      </CardContent>
+      <CardContent className="pt-0">
+        <Button className="w-full bg-primary text-white text-lg">Learn More</Button>
+      </CardContent>
+    </Card>
+  );
+}
+
+export default ServiceCard;
