88212f0a291bed2723165d4eb2ef98020faf9310
Diego <diego.boost@gmail.com>
2025-01-03 17:33:05 +0100

    badges

--- components/About.tsx
+++ components/About.tsx
@@ -27,7 +27,10 @@ export default function About() {
       id="about"
     >
       <div className="container mx-auto px-4 md:px-6 py-1 md:py-4">
-        <div className="flex flex-col items-center space-y-5 text-center mb-12">
+        <div className="flex flex-col items-center space-y-4 text-center mb-12">
+          <Badge variant="outline" className="text-primary border-primary mt-3 md:mt-4 px-4 py-1 rounded-full text-sm md:text-md">
+            About Us
+          </Badge>
           <h2 className="bg-gradient-to-r from-secondary to-primary text-transparent bg-clip-text text-4xl font-bold tracking-tighter md:text-5xl lg:text-6xl">
             Welcome to MyClinic
           </h2>
--- components/Services.tsx
+++ components/Services.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import services from "./utils/servicesData";
 import ServiceCard from "./utils/ServiceCard";
+import { Badge } from "./ui/badge";
 
 function Services() {
   const generalizedServices = services.filter(
@@ -17,7 +18,10 @@ function Services() {
       id="services"
     >
       <div className="container mx-auto px-4 md:px-6 py-1 md:py-4">
-        <div className="flex flex-col items-center space-y-5 text-center mb-8">
+        <div className="flex flex-col items-center space-y-4 text-center mb-8">
+          <Badge variant="outline" className="text-primary border-primary mt-3 md:mt-4 px-4 py-1 rounded-full text-sm md:text-md">
+            Healthcare Solutions
+          </Badge>
           <h2 className="bg-gradient-to-r from-secondary to-primary text-transparent bg-clip-text text-4xl font-bold tracking-tighter md:text-5xl lg:text-6xl">
             Services
           </h2>
