00b23c3aef443d906a8b11553cd83857fe897b6d
Diego <diego.boost@gmail.com>
2025-01-14 13:50:14 +0100

    debugging

--- app/auth/confirm/route.ts
+++ app/auth/confirm/route.ts
@@ -19,6 +19,7 @@ export async function GET(request: NextRequest) {
     })
     if (!error) {
       // redirect user to specified redirect URL or root of app
+      console.log(next)
       redirect(next)
     }
   }
